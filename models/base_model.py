4ccf27f9edcc574ddfbd04aef24ad6f67dfc7739
Joseph Kakai <josephngalu96@gmail.com>
2023-02-11 04:48:49 -0800

    Updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,57 @@
-#!/usr/bin/python3                                                        
-                                                                          
-import uuid                                                               
-from datetime import datetime                                             
-                                                                          
-class BaseModel:                                                          
-    """ a class that defines attribute id"""                              
-                                                                          
-    def __init__(self):                                                   
-        """constructor for class attribute id created_at, updated_at and m
-ethod"""                                                                  
-        self.id = str(uuid.uuid4())                                       
-        self.created_at = datetime.now()                                  
-        self.updated_at = datetime.now()                                  
-                                                                          
-    def __str__(self):                                                    
-        """returns string representation of aclass"""                     
-                                                                          
-        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
-)                                                                         
-                                                                          
-    def save(self):                                                       
-        """updates the updated attributes"""                              
-                                                                          
-        self.update_at = datetime.now()                                   
-                                                                          
+#!/usr/bin/python3
+"""A Base class with common methods/attributes
+   for other classes
+"""
+import uuid
+from datetime import datetime, time, date
+from models import storage
+
+
+class BaseModel:
+    """a class that defines attributes id,
+    created_at, updated_at and methods
+     """
+
+    def __init__(self, *args, **kwargs):
+    """constructor for class attrs id
+    created_at and updated_at
+    """
+
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()    
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.\
+                                strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+
+
+    def __str__(self):
+                                                                               """returns a string repr of the class
+        """
+
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+    """updates the updated_at attr
+    """
+
+    self.updated_at = datetime.now()
+    storage.save()
+
     def to_dict(self):                                                    
-        """returns dictionary representationof an instance"""             
-                                                                          
+        """returns dictionary representationof an instance
+        """             
+
         my_dict = self.__dict__.copy()                                    
-        my_dict["__class__"] = self.__class__.__name__,                   
-        my_dict["updated_at"] = self.updated_at.isoformat(),              
-        my_dict["created_at"] = self.created_at.isoformat()               
+        my_dict.update({
+            "__class__" : self.__class__.__name__,                   
+            "updated_at" : self.updated_at.isoformat(),              
+            "created_at" : self.created_at.isoformat()  
+            })
         return my_dict 
